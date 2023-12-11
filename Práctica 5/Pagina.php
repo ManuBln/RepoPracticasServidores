6d48dc554eaa9a4e0ecc5a5a2f625e4751d19527
Manuel <manu320cr@gmail.com>
2023-12-11 17:59:08 +0100

    final

--- Práctica 5/Pagina.php
+++ Práctica 5/Pagina.php
@@ -10,102 +10,103 @@
 </head>
 
 <body>
+    <?php
+    session_start();
+    ?>
 
 
 
 
+    <div id="contenedorRuta" class="container text-center mt-5">
+        <?php
+        echo '<p class="ruta">Fecha: ' . date('Y-m-d H:i:s') . '</p>';
+        echo '<p class="ruta">La ruta actual es la siguiente: ' . getcwd() . '</p>';
 
-        <div id="contenedorRuta" class="container text-center mt-5">
-            <?php
-            echo '<p class="ruta">Fecha: ' . date('Y-m-d H:i:s') . '</p>';
-            echo '<p class="ruta">La ruta actual es la siguiente: ' . getcwd() . '</p>';
-
-            ?>
-        </div>
-
+        ?>
+    </div>
 
 
-        <div class="contenedorBuscador">
 
+    <div class="contenedorBuscador">
+
+        <form action="Pagina.php" method="post">
+
+            <label for="buscador" meth>Indique el nombre del archivo: </label><br>
+            <input type="text" id="buscador" name="buscador"><br><br>
+
+            <input type="submit" value="Send" name="send">
+        </form>
+
+        <?php
+        if (isset($_POST['buscador'])) {
+            $nombre_archivo = $_POST['buscador'];
+            $directorio_actual = getcwd();
+            $archivos = scandir($directorio_actual);
+
+            if (in_array($nombre_archivo, $archivos)) {
+                echo "El archivo '$nombre_archivo' está en el directorio actual.";
+            } else {
+                echo "El archivo '$nombre_archivo' no está en el directorio actual.";
+            }
+        }
+        ?>
+
+
+    </div>
+
+
+    <?php
+
+    if ($_SESSION['rol'] == 'admin') {
+
+        ?>
+
+
+        <div id="contenedorCrear">
             <form action="Pagina.php" method="post">
 
-                <label for="buscador" meth>Indique el nombre del archivo: </label><br>
-                <input type="text" id="buscador" name="buscador"><br><br>
+                <label for="crear" meth>Indique el nombre del archivo para crearlo: </label><br>
+                <input type="text" id="crear" name="crear"><br><br>
+
+                <label for="contenido" meth>Indique el contenido: </label><br>
+                <input type="text" id="contenido" name="contenido"><br><br>
+
+                <label for="permisos" meth>Indique Permisos en octal: </label><br>
+                <input type="text" id="permisos" name="permisos"><br><br>
 
                 <input type="submit" value="Send" name="send">
             </form>
 
             <?php
-            if (isset($_POST['buscador'])) {
-                $nombre_archivo = $_POST['buscador'];
-                $directorio_actual = getcwd();
-                $archivos = scandir($directorio_actual);
 
-                if (in_array($nombre_archivo, $archivos)) {
-                    echo "El archivo '$nombre_archivo' está en el directorio actual.";
+            if (isset($_POST['crear'])) {
+                $crear = $_POST['crear'];
+                $contenido = $_POST['contenido'];
+                $permisos = intval($_POST['permisos'], 8);
+
+                $directorio_actual_2 = getcwd();
+                $url_archivo = $directorio_actual_2 . '/' . $crear;
+
+                if (file_put_contents($url_archivo, $contenido) !== false) {
+                    chmod($url_archivo, $permisos);
+                    echo "El archivo '$crear' se ha creado correctamente.";
                 } else {
-                    echo "El archivo '$nombre_archivo' no está en el directorio actual.";
+                    echo "No se pudo crear el archivo.";
                 }
             }
             ?>
 
-
         </div>
-
-
         <?php
-        if (!$_SESSION['rol'] = 'cliente1') {
 
-            ?>
+    }
+    ?>
 
-
-            <div id="contenedorCrear">
-                <form action="Pagina.php" method="post">
-
-                    <label for="crear" meth>Indique el nombre del archivo para crearlo: </label><br>
-                    <input type="text" id="crear" name="crear"><br><br>
-
-                    <label for="contenido" meth>Indique el contenido: </label><br>
-                    <input type="text" id="contenido" name="contenido"><br><br>
-
-                    <label for="permisos" meth>Indique Permisos en octal: </label><br>
-                    <input type="text" id="permisos" name="permisos"><br><br>
-
-                    <input type="submit" value="Send" name="send">
-                </form>
-
-                <?php
-
-                if (isset($_POST['crear'])) {
-                    $crear = $_POST['crear'];
-                    $contenido = $_POST['contenido'];
-                    $permisos = intval($_POST['permisos'], 8);
-
-                    $directorio_actual_2 = getcwd();
-                    $url_archivo = $directorio_actual_2 . '/' . $crear;
-
-                    if (file_put_contents($url_archivo, $contenido) !== false) {
-                        chmod($url_archivo, $permisos);
-                        echo "El archivo '$crear' se ha creado correctamente.";
-                    } else {
-                        echo "No se pudo crear el archivo.";
-                    }
-                }
-                ?>
-
-            </div>
-         <?php
-
-        }
-
-
-         ?>
-
-        <div class = " text-center">
-            <form action="logout.php">
-                <input type="submit" value="Cerrar Session">
-            </form>
-        </div>
+    <div class=" text-center">
+        <form action="logout.php">
+            <input type="submit" value="Cerrar Session">
+        </form>
+    </div>
 
 
 
