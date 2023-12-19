f99368f15fef7172fd426287d0139c7c216c938c
Manuel <manu320cr@gmail.com>
2023-12-19 17:01:16 +0100

    finalizado PracticaLaravel

--- PracticaLaravel/routes/web.php
+++ PracticaLaravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ControladorAnimales;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dameAnimales', [ControladorAnimales::class, 'getAnimales']);
+
+
